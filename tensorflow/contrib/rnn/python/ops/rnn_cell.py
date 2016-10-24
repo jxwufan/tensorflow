d3f0fcdec381da1d931fe6bfd213c9649a23b8f5
fan wu <jxwufan@gmail.com>
2016-10-24 21:53:03 +1100

    Add fast weight

--- tensorflow/contrib/rnn/python/ops/rnn_cell.py
+++ tensorflow/contrib/rnn/python/ops/rnn_cell.py
@@ -1238,3 +1238,115 @@ class LayerNormBasicLSTMCell(rnn_cell.RNNCell):
 
       new_state = rnn_cell.LSTMStateTuple(new_c, new_h)
       return new_h, new_state
+
+class LayerNormFastWeightsBasicRNNCell(rnn_cell.RNNCell):
+
+  def __init__(self, num_units, forget_bias=1.0, reuse_norm=False,
+               input_size=None, activation=nn_ops.relu,
+               layer_norm=True, norm_gain=1.0, norm_shift=0.0,
+               loop_steps=1, decay_rate=0.9, learning_rate=0.5,
+               dropout_keep_prob=1.0, dropout_prob_seed=None):
+
+    if input_size is not None:
+      logging.warn("%s: The input_size parameter is deprecated.", self)
+
+    self._num_units = num_units
+    self._activation = activation
+    self._forget_bias = forget_bias
+    self._reuse_norm = reuse_norm
+    self._keep_prob = dropout_keep_prob
+    self._seed = dropout_prob_seed
+    self._layer_norm = layer_norm
+    self._S = loop_steps
+    self._eta = learning_rate
+    self._lambda = decay_rate
+    self._g = norm_gain
+    self._b = norm_shift
+
+    @property
+    def state_size(self):
+      return self._num_units
+
+    @property
+    def output_size(self):
+      return self._num_units
+
+  def _norm(self, inp, scope=None):
+    with vs.variable_scope(scope or "Norm") as scope:
+      normalized = layers.layer_norm(inp, reuse=self._reuse_norm, scope=scope)
+      return normalized
+
+  def _linear(args, output_size, bias, bias_start=0.0, scope=None):
+    """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
+
+    Args:
+      args: a 2D Tensor or a list of 2D, batch x n, Tensors.
+      output_size: int, second dimension of W[i].
+      bias: boolean, whether to add a bias term or not.
+      bias_start: starting value to initialize the bias; 0 by default.
+      scope: VariableScope for the created subgraph; defaults to "Linear".
+
+    Returns:
+      A 2D Tensor with shape [batch x output_size] equal to
+      sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+
+    Raises:
+      ValueError: if some of the arguments has unspecified or wrong shape.
+    """
+    if args is None or (nest.is_sequence(args) and not args):
+      raise ValueError("`args` must be specified")
+    if not nest.is_sequence(args):
+      args = [args]
+
+    # Calculate the total size of arguments on dimension 1.
+    total_arg_size = 0
+    shapes = [a.get_shape().as_list() for a in args]
+    for shape in shapes:
+      if len(shape) != 2:
+        raise ValueError("Linear is expecting 2D arguments: %s" % str(shapes))
+      if not shape[1]:
+        raise ValueError("Linear expects shape[1] of arguments: %s" % str(shapes))
+      else:
+        total_arg_size += shape[1]
+
+    dtype = [a.dtype for a in args][0]
+
+    # Now the computation. Matrix will be used multiple times in fast weights rnn, so reuse is True.
+    with vs.variable_scope(scope or "Linear", reuse=True):
+      matrix = vs.get_variable(
+        "Matrix", [total_arg_size, output_size], dtype=dtype)
+      if len(args) == 1:
+        res = math_ops.matmul(args[0], matrix)
+      else:
+        res = math_ops.matmul(array_ops.concat(1, args), matrix)
+      if not bias:
+        return res
+      bias_term = vs.get_variable(
+        "Bias", [output_size],
+        dtype=dtype,
+        initializer=init_ops.constant_initializer(
+          bias_start, dtype=dtype))
+    return res + bias_term
+
+  def __call__(self, inputs, state, fast_weights, scope=None):
+    with vs.variable_scope(scope or type(self).__name__) as scope:
+      """Compute Wh(t)+Cx(t)"""
+      linear = self._linear([inputs, state], self._num_units, False)
+      """Compute h_0(t+1) = f(Wh(t)+Cx(t)"""
+      if self._reuse_norm:
+        h = self._activation(self._norm(linear, scope="Norm0"))
+      else:
+        h = self._activation(self._norm(linear))
+      for i in range(self._S):
+        """Compute h_{s+1}(t+1)=f([Wh(t)+Cx(t)]+A(t)h_s(t+1)) S times"""
+        if self._reuse_norm:
+          h = self._activation(self._norm(linear +
+                                          math_ops.matmul(fast_weights, h), scope="Norm%d" % (i + 1)))
+        else:
+          h = self._activation(self._norm(linear +
+                                          math_ops.matmul(fast_weights, h)))
+
+      """Compute A(t+1)"""
+      new_fast_weights = self._lambda * fast_weights + self._eta * math_ops.matmul(state, state, transpose_a=True)
+
+      return h, new_fast_weights
